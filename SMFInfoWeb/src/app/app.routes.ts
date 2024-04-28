c2e04a10810929866bfa1a4e11693dc21b949c5e
Jacek Grabowski <jacek@web.katowice.pl>
2024-04-28 12:05:52 +0200

    main-routing

--- SMFInfoWeb/src/app/app.routes.ts
+++ SMFInfoWeb/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './views/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainComponent}
+];

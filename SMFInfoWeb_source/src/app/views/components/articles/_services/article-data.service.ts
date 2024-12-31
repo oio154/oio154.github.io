1751bf15290bae06e835785e58f346899aad9f5e
Jacek <jacek@web.katowice.pl>
2024-12-31 11:04:47 +0100

    happy2025

--- SMFInfoWeb_source/src/app/views/components/articles/_services/article-data.service.ts
+++ SMFInfoWeb_source/src/app/views/components/articles/_services/article-data.service.ts
@@ -10,9 +10,9 @@ export class ArticleDataService {
 
   data: UserArticle[] = [
     {
-      id: "oidsjf-387ch-sdaus7-d-iuhiudshf87",
-      title: "Zmiany infrastruktury",
-      tx: "W czasie długiego weekendu w Państwa firmie prowadzone będą zmiany infrastruktury sieciowej, programowej i komputerowej. Może się to wiązać - mam nadzieję z jak najmniejszymi -utrudnieniami w dostępie do zasobów informatycznych. W celu upłynnienia pracy i umożliwienia szybkiego reagowania, na ten czas, na dole tej strony załączono ikonę czatu. Czatu niniejszego mogą używać wszyscy pracownicy Spółdzielni. Można za jego pośrednictwem zgłosić bezpośrednio do obsługi wszelkie błędy i niedogodności a także uzyskać odpowiedzi na wszelkie pytania dotyczące infrastruktury informatycznej, proszę jednak nie przesyłać za pomocą czatu żadnych danych osobowych. Można się przedstawić wyłącznie imieniem, loginem a nawet wewnętrznym numerem telefonu."
+      id: "OIDJF-388D88-IODSJF8-OSDIF7",
+      title: "2025",
+      tx: "<p>Szczęśliwego nowego roku</p><img src=\"/assets/ai-generated-9147506_1280.jpg\">"
     },
   ];
 
